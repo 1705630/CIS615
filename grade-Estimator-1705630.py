8d82baa5ecc1d28679c1aafb61a86d81a837668b
1705630 <156516680+1705630@users.noreply.github.com>
2024-01-15 04:02:53 +0300

    Add files via upload
    This file is for grade-Estimator Project.

---
+++ grade-Estimator-1705630.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#initializing the python environment 
+#Prompting students to input first name
+first_name= input("Enter first name:")
+#prompting students to input second name
+last_name= input("Enter last name:")
+
+#combining the two names to single lines 
+Names = first_name + "" +last_name
+
+#outputing the input variables
+print(Names)
+
+
+# In[ ]:
+
+
+
+
